d8333ac930a351338bf64e9f5b84a67f1b8d8b0f
Ryan Joya <ryanjoya@users.noreply.github.com>
2016-03-21 18:18:20 -0500

    Create 3.02 multiple variables.js

---
+++ 3.02 multiple variables.js
@@ -0,0 +1,11 @@
+var score1 = 23;
+var score2 = 25;
+var score3 = 21;
+
+var sum = score1 + score2 + score3;
+var average = sum / 3;
+
+var range = score2 - score3;
+
+var fraction = 23 / 30;
+var percentage = fraction * 100;
