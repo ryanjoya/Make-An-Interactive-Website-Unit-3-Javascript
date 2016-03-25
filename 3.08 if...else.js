37d6cff4d2e992f03a1d68f319104d1e34d16a06
Ryan Joya <ryanjoya@users.noreply.github.com>
2016-03-24 19:52:47 -0500

    Create 3.08 if...else.js

---
+++ 3.08 if...else.js
@@ -0,0 +1,12 @@
+var myName = "Michaelangelo";
+var status;
+
+if( myName.length >= 15 ) {
+    status = "You have a very long name!";
+}
+else if( myName.length >= 7 ) {
+    status = "You have a long name!";
+}
+else {
+    status = "You have a short name!";
+}
